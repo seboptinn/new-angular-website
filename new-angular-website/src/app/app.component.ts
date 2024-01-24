73b92d16a67e00578f6466f74591777137b6d16c
Sebastian Dahl <sebastian.dahl@optinn.no>
2024-01-24 21:13:56 +0100

    Plain navigation added and css removed

--- new-angular-website/src/app/app.component.ts
+++ new-angular-website/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './pages/home/container/home.component';
-import { AboutComponent } from './pages/about/container/about.component';
-import { ContactComponent } from './pages/contact/container/contact.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { HomeComponent } from "./pages/home/container/home.component";
+import { AboutComponent } from "./pages/about/container/about.component";
+import { ContactComponent } from "./pages/contact/container/contact.component";
+import { ProjectsComponent } from "./pages/projects/projects.component";
+import { NavigationBarComponent } from "./shared/components/navigation-bar/navigation-bar.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [
     CommonModule,
@@ -16,10 +17,11 @@ import { ProjectsComponent } from './pages/projects/projects.component';
     AboutComponent,
     ContactComponent,
     ProjectsComponent,
+    NavigationBarComponent,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'nettside-native';
+  title = "nettside-native";
 }
---
+++ new-angular-website/src/app/shared/components/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "navigation-bar",
+  standalone: true,
+  imports: [],
+  templateUrl: "./navigation-bar.component.html",
+  styleUrl: "./navigation-bar.component.scss",
+})
+export class NavigationBarComponent {}
