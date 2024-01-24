bee3df3f61b9515b9556ffa57dc5d07fbfd9edca
Sebastian Dahl <sebastian.dahl@optinn.no>
2024-01-24 23:19:54 +0100

    Formatting

--- new-angular-website/src/app/projects/project-one/container/project-one.component.ts
+++ new-angular-website/src/app/projects/project-one/container/project-one.component.ts
@@ -14,11 +14,18 @@ import { SharedModule } from "../../../shared/shared.module";
 })
 export class ProjectOneComponent {
   @Input() public salary = 700000;
-  public salaryPerMonth = this.salary / 12;
-  public salaryPerDay = this.salaryPerMonth / 30;
-  public salaryPerHour = this.salaryPerDay / 7.5;
-  public salaryPerMinute = this.salaryPerHour / 60;
-  public salaryPerSecond = this.salaryPerMinute / 60;
+  public workHoursPerYear = 1950;
+  public hourRate = this.salary / this.workHoursPerYear;
+  public monthRate = this.hourRate * 162.5;
+  public dayRate = this.hourRate * 7.5;
+  public minuteRate = this.hourRate / 60;
+  public secondRate = this.minuteRate / 60;
+
+  // public salaryPerMonth = this.salary / 12;
+  // public salaryPerDay = this.salaryPerMonth / 22;
+  // public salaryPerHour = this.salaryPerDay / 7.5;
+  // public salaryPerMinute = this.salaryPerHour / 60;
+  // public salaryPerSecond = this.salaryPerMinute / 60;
 
   public currentPay = 0;
 
@@ -26,9 +33,16 @@ export class ProjectOneComponent {
 
   constructor() {
     const seconds = interval(1000);
-
     seconds.pipe(timeInterval()).subscribe((value) => {
-      this.currentPay += this.salaryPerSecond;
+      this.currentPay += this.secondRate;
     });
+  }
+
+  public calculate(): void {
+    this.hourRate = this.salary / this.workHoursPerYear;
+    this.monthRate = this.hourRate * 162.5;
+    this.dayRate = this.hourRate * 7.5;
+    this.minuteRate = this.hourRate / 60;
+    this.secondRate = this.minuteRate / 60;
   }
 }
